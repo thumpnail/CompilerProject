1572bcff29dd19cc9ffd7812d74a47c2a3e717a1
Fenex <31255152+thumpnail@users.noreply.github.com>
2025-05-23 16:22:22 +0200

    test parser nano

--- NanoScript.Tiny/Parseus.NanoScript.cs
+++ NanoScript.Tiny/Parseus.NanoScript.cs
@@ -1,84 +1,68 @@
 using NanoScript.Parser;
+
 using Parseus.Parser.Implicit;
+
 using NanoScript.Parser.AstNodes;
+
 using static NanoScript.Token;
+
 using Parseus.Parser.ObjectBased;
 
 public class NanoScriptParser : BaseParser {
-	public override ModuleStatement Parse(string src) {
+	public record NanoProgram {}
+	public record NanoStatement {}
+
+	public override NanoProgram Parse(string src) {
 		return new();
 	}
-	private static Parser<ModuleStatement> ModuleStatementParser = new((c, self) => {
-		Token(c, MOD.ToString(), out _);
-		Node(c, IdentifierExpressionParser, out self.moduleName);
-		Opt(c, ctx => {
-			Repeat(c, ctx => {
-				Node(c, ImportStatementParser, out var importStatement);
-			});
-		});
-		Opt(c, ctx => {
-			Alt(c, c => {
-					Token(c, LEFTBRACE.ToString(), out _);
-					Node(c, StatementsParser, out var statements);
-					Token(c, RIGHTBRACE.ToString(), out _);
-					self.hasBody = true;
-				},
-				c => {
-					Node(c, StatementsParser, out var statements);
-				});
-		});
+	// program         = { statement } ;
+	private static Parser<NanoProgram> nanoProgramParser = new((c, self) => {
+
 	});
-	private static Parser<List<ModuleStatement>> ModuleStatementsParser = new((ctx, self) => {
-		Opt(ctx, ctx => {
-			Repeat(ctx, ctx => {
-				Node(ctx, ModuleStatementParser, statement => {
-					self.Add(statement);
-				});
-			});
-		});
-	});
-	private static Parser<List<IStatement>> StatementsParser = new((ctx, self) => {});
-	private static Parser<List<ImportStatement>> ImportStatementsParser = new((ctx, self) => {});
-	private static Parser<ImportStatement> ImportStatementParser = new((ctx, self) => {});
-	//private static Parser<IStatement> StatementParser = new((ctx, self) => {});
-	private static Parser<VariableDeclarationStatement> VariableDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<StructDeclarationStatement> StructDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<ClassDeclarationStatement> ClassDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<FunctionDeclarationStatement> FunctionDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<List<ParameterDeclaration>> ParameterDeclarationListParser = new((ctx, self) => {});
-	private static Parser<FunctionCallStatement> FunctionCallStatementParser = new((ctx, self) => {});
-	private static Parser<ReturnStatement> ReturnStatementParser = new((ctx, self) => {});
-	private static Parser<EnumDeclarationStatement> EnumDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<EnumValueDeclaration> EnumValueDeclarationParser = new((ctx, self) => {});
-	private static Parser<AssignmentStatement> AssignmentStatementParser = new((ctx, self) => {});
-	private static Parser<ConditionalStatement> ConditionalStatementParser = new((ctx, self) => {});
-	private static Parser<SwitchStatement> SwitchStatementParser = new((ctx, self) => {});
-	private static Parser<SubSwitchStatement> SubSwitchStatementParser = new((ctx, self) => {});
-	private static Parser<List<SubSwitchStatement>> SubSwitchStatementsParser = new((ctx, self) => {});
-	private static Parser<ForStatement> ForStatementParser = new((ctx, self) => {});
-	private static Parser<ErrorStatement> ErrorStatementParser = new((ctx, self) => {});
-	private static Parser<BreakContinueStatement> BreakContinueStatementParser = new((ctx, self) => {});
-	private static Parser<InterfaceStatement> InterfaceStatementParser = new((ctx, self) => {});
-	private static Parser<UnionStatement> UnionStatementParser = new((ctx, self) => {});
-	private static Parser<DeclarationStatement> DeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<AssertionStatement> AssertionStatementParser = new((ctx, self) => {});
-	private static Parser<LabelStatement> LabelStatementParser = new((ctx, self) => {});
-	private static Parser<GotoStatement> GotoStatementParser = new((ctx, self) => {});
-	private static Parser<TypeDeclarationStatement> TypeDeclarationStatementParser = new((ctx, self) => {});
-	private static Parser<ArrayCreationExpression> ArrayCreationExpressionParser = new((ctx, self) => {});
-	private static Parser<ArrayIndexingExpression> ArraylndexingExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> BaseExpressionParser = new((ctx, self) => {});
-	private static Parser<BooleanExpression> BooleanExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> ExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> FactorExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> FullExpressionParser = new((ctx, self) => {});
-	private static Parser<FunctionCallExpression> FunctionCallExpressionParser = new((ctx, self) => {});
-	private static Parser<IdentifierExpression> IdentifierExpressionParser = new((ctx, self) => {});
-	private static Parser<ExpressionList> ListExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> LiteralExpressionParser = new((ctx, self) => {});
-	private static Parser<NumberExpression> NumberExpressionParser = new((ctx, self) => {});
-	private static Parser<StringExpression> StringExpressionParser = new((ctx, self) => {});
-	//private static Parser<IExpression> TermExpressionParser = new((ctx, self) => {});
-	private static Parser<TypeConversionExpression> TypeConversionExpressionParser = new((ctx, self) => {});
-	private static Parser<UnaryExpression> UnaryExpressionParser = new((ctx, self) => {});
+	// program         = { statement } ;
+
+	// statement       = variable_declaration
+	//                 | constant_declaration
+	//                 | operation
+	//                 | function_definition
+	//                 | function_call
+	//                 | jump_statement
+	//                 | conditional_statement
+	//                 | loop_statement
+	//                 | struct_definition
+	//                 | package_definition
+	//                 | include_statement
+	//                 | destruct_statement
+	//                 | error_statement ;
+	// variable_declaration = "let" identifier value { value } ;
+	// constant_declaration = "cst" identifier value { value } ;
+	// value           = identifier
+	//                 | number
+	//                 | string
+	//                 | character
+	//                 | array_definition ;
+	// array_definition = "[" value { "," value } "]" ;
+	// operation       = identifier identifier operator identifier ;
+	// operator        = "+" | "-" | "*" | "/" ;
+	// function_definition = "fnc" identifier [ parameter_list ] block "ret" [ return_value { return_value } ] ;
+	// parameter_list  = identifier { identifier } ;
+	// return_value    = identifier | value ;
+	// function_call   = "cll" identifier { identifier | value } ;
+	// jump_statement  = "jmp" identifier | ":" identifier ;
+	// conditional_statement = "iff" condition block [ "elf" condition block ] [ "els" block ] "ext" ;
+	// condition       = identifier logical_operator identifier ;
+	// logical_operator = "EQL" | "LES" | "GRT" | "TRU" | "FLS" ;
+	// loop_statement  = "whl" condition block "ext"
+	//                 | "for" identifier value condition value value block "ext"
+	//                 | "for" identifier identifier block "ext" ;
+	// struct_definition = "tbl" identifier [ parameter_list ] block "ext" ;
+	// package_definition = "pck" identifier block "ext" ;
+	// include_statement = "inc" identifier [ identifier ] ;
+	// destruct_statement = "~" identifier ;
+	// error_statement = "err" number [ string ] ;
+	// block           = { statement } ;
+	// identifier      = ? any valid identifier ? ;
+	// number          = ? any valid number ? ;
+	// string          = '"' ? any valid string content ? '"' ;
+	// character       = "'" ? any valid character ? "'" ;
 }
