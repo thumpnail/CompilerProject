aca699f4ea68799bbba7f85eda70c83cc4c6d785
Fenex <31255152+thumpnail@users.noreply.github.com>
2025-07-19 13:44:44 +0200

    Refactor parser structure and clean up EBNF syntax
    Refactored ParserV2 to introduce interfaces and classes for statements and expressions, and added a stub for statement parsing. Removed commented-out grammar and implementation notes from the original parser. Cleaned up the EBNF syntax file by removing placeholder rules and comments for expression, identifier, and statement, consolidating grammar definitions.

--- NanoScript/src/NanoScript/ParserV2/Parser.cs
+++ NanoScript/src/NanoScript/ParserV2/Parser.cs
@@ -1,5 +1,22 @@
+using Parseus.Parser.Implicit;
 namespace NanoScript.ParserV2;
 
-public class Parser {
-	
+public partial class Parser : BaseParser {
+	interface IStatement;
+	interface IExpression;
+	class Statement {
+		public IStatement internalStatement;
+	}
+	class Expression {
+		public IExpression internalExpression;
+	}
+	class IfStatement : IStatement {
+		public List<(Expression? condition,Statement block)> conditions = new();
+	}
+	private static Parser<Statement> StatementParser = new((c, self) => {
+		
+	});
+	public override object Parse(string src) {
+		throw new NotImplementedException();
+	}
 }
